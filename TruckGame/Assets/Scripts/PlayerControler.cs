109ba18213d5dbdaf2ddd83388b62c812bf79d92
Priscila Rocha <priscila.mizuiky@gmail.com>
2021-09-11 09:43:44 -0300

    Adding speed to the player

--- TruckGame/Assets/Scripts/PlayerControler.cs
+++ TruckGame/Assets/Scripts/PlayerControler.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class PlayerControler : MonoBehaviour
 {
+    [SerializeField]
+    private float playerSpeed;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -16,11 +19,11 @@ public class PlayerControler : MonoBehaviour
         //Moves the vehicle forward
         if(Input.GetKey(KeyCode.W))
         {
-            this.transform.position += Vector3.forward * Time.deltaTime * 15;
+            this.transform.position += Vector3.forward * Time.deltaTime * playerSpeed;
         }
         else if(Input.GetKey(KeyCode.S))
         {
-            this.transform.position -= Vector3.forward * Time.deltaTime * 15;
+            this.transform.position -= Vector3.forward * Time.deltaTime * playerSpeed;
         }
         
     }
