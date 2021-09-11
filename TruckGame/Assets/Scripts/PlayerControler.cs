11638cb8c64534ddd4ed3afd04e3a6c62a7ac4d6
Priscila Rocha <priscila.mizuiky@gmail.com>
2021-09-11 09:26:16 -0300

    Adding Player Controller and the player foward movement

---
+++ TruckGame/Assets/Scripts/PlayerControler.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControler : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Moves the vehicle forward
+        if(Input.GetKey(KeyCode.W))
+        {
+            this.transform.position += Vector3.forward * Time.deltaTime * 15;
+        }
+        else if(Input.GetKey(KeyCode.S))
+        {
+            this.transform.position -= Vector3.forward * Time.deltaTime * 15;
+        }
+        
+    }
+}
