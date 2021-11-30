7fca5b6e28129d45e75459de618598126803e81a
Priscila da Rocha Alves <priscila.mizuiky@gmail.com>
2021-11-30 08:55:45 -0300

    Making the propeller spin constantly + adjusting some properties in the project

--- TruckGame/Assets/Challenge 1/Scripts/PlayerControllerX.cs
+++ TruckGame/Assets/Challenge 1/Scripts/PlayerControllerX.cs
@@ -6,12 +6,18 @@ public class PlayerControllerX : MonoBehaviour
 {
     public float speed;
     public float rotationSpeed;
+    public float propellerRotation;
+
     private float horizontalInput;
+    private float verticalInput;
+
+    [SerializeField]
+    private GameObject planePropeller;
 
     // Start is called before the first frame update
     void Start()
     {
-
+   
     }
 
     // Update is called once per frame
@@ -19,11 +25,17 @@ public class PlayerControllerX : MonoBehaviour
     {
         // get the user's vertical input
         horizontalInput = Input.GetAxis("Horizontal");
+        verticalInput = Input.GetAxis("Vertical");      
 
         // move the plane forward at a constant rate
         transform.Translate(Vector3.forward * Time.deltaTime * speed * horizontalInput);
 
+
         // tilt the plane up/down based on up/down arrow keys
-        //transform.Rotate(Vector3.right * rotationSpeed * Time.deltaTime);
+        transform.Rotate(Vector3.right * Time.deltaTime * rotationSpeed * verticalInput);
+       
+          
+        //rotate constantly the plane propeller
+        this.planePropeller.transform.RotateAround(this.planePropeller.transform.position, Vector3.forward, propellerRotation * Time.deltaTime);
     }
 }
